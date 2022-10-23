ebc24bcdedebefee032e25d295f1cd94044bd287
B1lkao <hrabousolesia@gmail.com>
2022-10-23 11:51:35 +0300

    Змінила друге завдання

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,19 +1,18 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+console.log(galleryItems);
 
-
-const galaryDiv = document.querySelector('.gallery');
-
-const createGalery = collection => {
-    collection.map(({ preview, original, description }) => {
-        const markingElement = `<a class="gallery__item" href="${original}">
+const render = galleryItems.reduce(
+  (acc, { description, original, preview }) =>
+    acc +
+    `<a class="gallery__item" href="${original}">
   <img class="gallery__image" src="${preview}" alt="${description}"/>
-</a>`;
-        galaryDiv.insertAdjacentHTML('afterbegin', markingElement);
-    })
-}
+</a>`,
+  ''
+);
 
-createGalery(galleryItems)
+const gallery = document.querySelector('.gallery');
+gallery.insertAdjacentHTML('afterbegin', render);
 
-
+let galleryNew = new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay: 250 });
